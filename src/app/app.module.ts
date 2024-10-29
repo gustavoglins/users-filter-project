e91c126c897f034ee052ba2f8b11b4e70a8ef881
gustavoglins <gustavoguimaraes.241025@gmail.com>
2024-10-28 21:08:31 -0300

    feat: add user-related interfaces and initial user list data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ComponentsModule } from './components/components.module';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
@@ -13,7 +12,6 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ComponentsModule
   ],
   providers: [
     provideClientHydration(),
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from "@angular/core";
 import { AngularMaterialModule } from "../angular-material/angular-material.module";
+import { UserDetailsComponent } from './user-details/user-details.component';
 
 @NgModule({
     declarations: [
 
-    ],
+    
+    UserDetailsComponent
+  ],
     imports: [
         AngularMaterialModule
     ],
@@ -13,5 +16,5 @@ import { AngularMaterialModule } from "../angular-material/angular-material.modu
     ],
 })
 export class ComponentsModule {
-    
+
 }
