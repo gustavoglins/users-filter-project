3255d0c9d643732675b14ef3d91de46ae857294b
gustavoglins <gustavoguimaraes.241025@gmail.com>
2024-10-29 10:01:33 -0300

    feat: create FilterComponent

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -1,17 +1,38 @@
 import { NgModule } from "@angular/core";
 
 import { MatListModule } from '@angular/material/list';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
 import { MatDividerModule } from '@angular/material/divider';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 
 @NgModule({
     imports: [
         MatListModule,
-        MatDividerModule
+        MatDividerModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatDatepickerModule,
+        MatSelectModule,
+        MatButtonModule
     ],
     exports: [
         MatListModule,
-        MatDividerModule
+        MatDividerModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatDatepickerModule,
+        MatSelectModule,
+        MatButtonModule
+    ],
+    providers: [
+        provideNativeDateAdapter()
     ],
 })
 export class AngularMaterialModule {
